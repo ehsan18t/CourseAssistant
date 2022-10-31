6d90c83e542904e6aeba6cf59b1d3211f386d001
mdashik313 <mdashik5360@gmail.com>
2022-10-31 10:32:23 +0600

    cleanup

--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html"],
+  content: ["./templates/public/index.html"],
   theme: {
     extend: {},
   },
