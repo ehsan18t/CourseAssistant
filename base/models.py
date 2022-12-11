ae182d733a25ab8db0dc48567e714b1ba4f0cbc8
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-11 23:22:46 +0600

    created models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    domain = models.CharField(max_length=300)
+    email_pattern = models.CharField(max_length=100)  # university id pattern
+    sid_pattern = models.CharField(max_length=100)  # student id pattern
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    s_id = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    passwords = models.CharField(max_length=300)
+    email = models.CharField(max_length=100)
+    type = models.CharField(max_length=20)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    periods = models.IntegerField()  # 1 = trimester, 2 = Semester
