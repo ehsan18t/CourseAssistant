89d8112031c62a9ebf1e5c25b4ae1278fe31a664
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-11 23:32:16 +0600

    added definition to models

--- base/models.py
+++ base/models.py
@@ -5,8 +5,11 @@ class University(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     domain = models.CharField(max_length=300)
-    email_pattern = models.CharField(max_length=100)  # university id pattern
+    email_pattern = models.CharField(max_length=100)  # university pattern
     sid_pattern = models.CharField(max_length=100)  # student id pattern
+
+    def __str__(self):
+        return f'{self.name} {self.domain} ({self.email_pattern}) - {self.sid_pattern}'
 
 
 class User(models.Model):
@@ -19,3 +22,6 @@ class User(models.Model):
     type = models.CharField(max_length=20)
     university = models.ForeignKey(University, on_delete=models.CASCADE)
     periods = models.IntegerField()  # 1 = trimester, 2 = Semester
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.s_id}) - {self.university.name}'
