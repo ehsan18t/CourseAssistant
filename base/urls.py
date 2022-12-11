502d64ebf0adfd3adb641038ed182d7b83319bfc
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-12 03:09:49 +0600

    added login (Partial)

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('messages', views.messages, name="messages"),
     path('chat', views.chat, name="chat"),
+    path('login', views.login, name="login"),
 ]
--- base/views.py
+++ base/views.py
@@ -9,6 +9,18 @@ def home(request):
     return render(request, 'home.html', {'data': data})
 
 
+def login(request):
+    email = request.POST.get('email')
+    passwords = request.POST.get('passwords')
+    if email and passwords:
+        user = User.objects.filter(email=email).values()
+        if user and user.passwords == passwords:
+            return render(request, 'home.html')
+        else:
+            return render(request, 'login.html', {'email': email, 'passwords': passwords})
+    return render(request, 'login.html')
+
+
 def messages(request):
     return render(request, 'messages.html')
 
