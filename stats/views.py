0f7872d0f0008c53a04e42f75f3d9cccb170cf16
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-16 16:09:23 +0600

    added stats styles

--- stats/views.py
+++ stats/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 
-# Create your views here.
-
 def stats(request):
-    return render(request, 'stats/stats.html')
+    data = {
+        '1': {'id': 1, 'name': 'Semester 1', 'start_date': '2014-01-01', 'end_date': '2014-05-01', 'is_running': False, 'auto_add_to_group': True},
+        '2': {'id': 2, 'name': 'Semester 2', 'start_date': '2014-06-01', 'end_date': '2014-12-01', 'is_running': True, 'auto_add_to_group': True},
+    }
+    return render(request, 'stats/stats.html', {'data': data})
