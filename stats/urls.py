466439f1a1d6b5a9382182b0e67587f3864459f9
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-16 23:14:04 +0600

    changed url patterns

--- stats/urls.py
+++ stats/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.stats, name="stats"),
-    path('semester=<int:pk>/', views.courses, name="courses"),
-    path('semester=<int:s_pk>/course=<int:c_pk>', views.assessments, name="assessments"),
+    path('semester=<int:pk>', views.courses, name="courses"),
+    path('semester=<int:s_pk>&course=<int:c_pk>', views.assessments, name="assessments"),
 ]
