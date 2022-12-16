03f46102f01311c8a377762a8254d335e3baeabb
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-16 18:39:18 +0600

    added option to add Semester

--- stats/views.py
+++ stats/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 
+from .models import Semester
+
 
 def stats(request):
-    data = {
-        '1': {'id': 1, 'name': 'Semester 1', 'start_date': '2014-01-01', 'end_date': '2014-05-01', 'is_running': False, 'auto_add_to_group': True},
-        '2': {'id': 2, 'name': 'Semester 2', 'start_date': '2014-06-01', 'end_date': '2014-12-01', 'is_running': True, 'auto_add_to_group': True},
-    }
+    if request.method == 'POST' and 'add_semester' in request.POST:
+        add_semester(request)
+    data = Semester.objects.all()
     return render(request, 'stats/stats.html', {'data': data})
+
+
+def add_semester(request):
+    semester = request.POST.get('semester_name')
+    start_date = request.POST.get('semester_start_date')
+    end_date = request.POST.get('semester_end_date')
+
+    is_running = request.POST.get('is_semester_running')
+    if is_running == 'on':
+        is_running = True
+    else:
+        is_running = False
+
+    auto_add_to_group = request.POST.get('auto_add_to_group')
+    if auto_add_to_group == 'on':
+        auto_add_to_group = True
+    else:
+        auto_add_to_group = False
+
+    obj = Semester(name=semester, start_date=start_date, end_date=end_date, is_running=is_running, auto_add_to_group=auto_add_to_group)
+    obj.save()
