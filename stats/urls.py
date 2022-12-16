0c2f0b931e20fb77d3aca88978e9426656e449ed
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-16 21:59:34 +0600

    added assessments view url

--- stats/urls.py
+++ stats/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.stats, name="stats"),
     path('semester=<int:pk>/', views.courses, name="courses"),
+    path('semester=<int:s_pk>/course=<int:c_pk>', views.assessments, name="assessments"),
 ]
