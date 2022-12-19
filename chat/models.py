278006fc825fd46a9b0bad4808c6f9ec610c7858
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-19 19:24:28 +0600

    added models for chat

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from base.models import User
 
-# Create your models here.
+# Models for the chat app
+class Chat(models.Model):
+    id = models.AutoField(primary_key=True)
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE)
+    last_message = models.CharField(max_length=1000, blank=True, null=True)
+    last_message_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user1} and {self.user2}'
+
+class Group(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    last_message = models.CharField(max_length=1000, blank=True, null=True)
+    last_message_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+# If private chat, 'chat' and 'receiver' field will be filled and 'group' field will be null
+# If group chat, 'group' field will be filled and 'chat' and 'receiver' field will be null
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, blank=True, null=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=1000)
+    time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.sender} : {self.message}'
+
+
