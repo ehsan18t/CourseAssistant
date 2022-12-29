990e6c005805287f3d7e12e79ed5a659ecbba51c
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-29 16:25:30 +0600

    added debug  filter to work url in debug mode

--- CourseAssistant/urls.py
+++ CourseAssistant/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from base.views import install_page
 
@@ -10,3 +12,6 @@ urlpatterns = [
     path('chat/', include('chat.urls')),
     path('install/', install_page, name='install'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
