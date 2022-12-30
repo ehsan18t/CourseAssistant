7da1f9dbd4fff57d1cee94e68225c9f839cb04c9
mdashik313 <mdashik5360@gmail.com>
2022-12-31 02:41:27 +0600

    profile edit and picture upload

--- base/urls.py
+++ base/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('login', views.login_page, name="login"),
     path('logout', views.logout_user, name="logout"),
     path('signup', views.signup, name="signup"),
-    path('profile', views.user_profile, name="profile")
+    path('profile', views.user_profile, name="profile"),
+    path('edit_profile', views.edit_profile, name="edit_profile"),
+    path('profile=<int:pk>', views.others_profile, name="others_profile")
 ]
