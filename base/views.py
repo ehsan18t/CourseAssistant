5f1c4eaed1cae57b5a05f926d9925016ba401036
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-31 00:42:20 +0600

    fixed course approval panel

--- base/views.py
+++ base/views.py
@@ -86,83 +86,9 @@ def chat(request):
     return render(request, 'chat.html')
 
 
-contexts = [
-        {
-        "course": "Datastructur and Algorithm",
-        "con": "Ashik ",
-        "file": "ct2.pdf",
-        "desc": "ct2, faculty: FTI asdfasd asdf a             asdfsdf asdf asdf asdf asdf asdf",
-        "id": "1"
-        },
-        {
-        "course": "System Analysis and Design",
-        "con": "Kamran",
-        "file": "slide1.pdf",
-        "desc": "covers lecture upto 3rd class",
-        "id": "3"
-        },
-        {
-        "course": "Computer Architercure",
-        "con": "Sharif ",
-        "file": "slide3.pdf",
-        "desc": "topics: Pipeline and MIPS code",
-        "id": "2"
-        },
-        {
-        "course": "System Analysis and Design",
-        "con": "Karim",
-        "file": "slide2.pdf",
-        "desc": "covers lecture upto 3rd class",
-        "id": "4"
-        },
-        {
-        "course": "Fundamenta Calc",
-        "con": "Ahsan",
-        "file": "slide1.pdf",
-        "desc": "covers lecture upto 3rd class",
-        "id": "5"
-        },
-        {
-        "course": "System Analysis and Design",
-        "con": "Ashik",
-        "file": "mid.pdf",
-        "desc": "midterm fall 2020",
-        "id": "6"
-        },
-        {
-        "course": "Microcontroller",
-        "con": "Asif",
-        "file": "presentation1.pdf",
-        "desc": "our presentation on Django",
-        "id": "7"
-        },
-        {
-        "course": "Microcontroller",
-        "con": "Asif",
-        "file": "presentation1.pdf",
-        "desc": "our presentation on Django",
-        "id": "8"
-        },
-        {
-        "course": "Microcontroller",
-        "con": "Asif",
-        "file": "presentation1.pdf",
-        "desc": "our presentation on Django",
-        "id": "9"
-        },
-        
-        {
-        "course": "Computer Architucture",
-        "con": "Tabarak",
-        "file": "mid.pdf",
-        "desc": "mid term question, fall 2020",
-        "id": "10"
-        }
-    ]
-
 def content_approval(request):
     
-    global contexts
+    contexts = Content.objects.filter(approved=False)
     if request.method == 'POST' :
         whichBtn = request.POST.get('btn')
         isAll = request.POST.getlist('allSelected')
@@ -172,11 +98,14 @@ def content_approval(request):
         if whichBtn == 'approve':
             print('approve is selected')
             for i in checkedItems:
-                contexts = [j for j in contexts if not ((j['id']) == i)]
+                context = Content.objects.get(id=i)
+                context.approved = True
+                context.save()
         else :
             print('delete is selected')
             for i in checkedItems:
-                contexts = [j for j in contexts if not ((j['id']) == i)]
+                context = Content.objects.get(id=i)
+                context.delete()
         
 
     paginator = Paginator(contexts, 4) # Show 1 data set per page.
